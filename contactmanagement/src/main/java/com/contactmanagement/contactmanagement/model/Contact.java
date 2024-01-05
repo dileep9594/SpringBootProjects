e5a6b96be65614a58d48fbd7d3dbce469b9a0a2a
Dileep Pandey <dileep9594@gmail.com>
2024-01-05 16:13:55 +0530

    models and dto's added

--- contactmanagement/src/main/java/com/contactmanagement/contactmanagement/model/Contact.java
+++ contactmanagement/src/main/java/com/contactmanagement/contactmanagement/model/Contact.java
@@ -5,14 +5,22 @@ import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import com.contactmanagement.contactmanagement.web.dto.ContactDto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Document(collection =  "contacts")
 @TypeAlias("contact")
-
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Contact {
  @Id
  String emailId ;
  @Field("firstName") 
- String firsName ;
+ String firstName ;
  @Field("middleName") 
  String middleName ;
  @Field("lastName") 
@@ -26,7 +34,9 @@ public class Contact {
  @Field("age")
  short age ;
 
- public Contact ()
+ public Contact (ContactDto contactdao){
+
+ } 
 
 
 
